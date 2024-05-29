988b7d2a1e1ff2a1062c7708944e31b6a1d27a47
SecroLoL <azshan@stanford.edu>
2024-05-28 21:09:32 -0700

    add prompting script for claude

---
+++ llm_prompt/constants.py
@@ -0,0 +1,2 @@
+DEFAULT_MOD_CONTEXT = "You are an admin on a social platform that is moderating content, specifically with attention towards ensuring that online terrorist recruitment material stays out of sight. Therefore, your goal is to observe activity on the platform and judge whether certain reported material is related to terrorist recruitment. "
+DEFAULT_CLASSIFICATION_PREP = "Is this message related to terrorist recruitment? Answer \"Yes\" or \"No\". Do not provide explanation for your answer.\n\n"
